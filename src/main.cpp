3bf78596fcde827ce87ee5a28edcd3fd44503b5a
Anjana Rajam <64962809+anjanarajam@users.noreply.github.com>
2020-08-29 17:18:20 +0200

    Bug Fix to avoid jerk and acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -198,9 +198,9 @@ int main() {
           /* Behavioral planning : what has to be done based on the predictions */
           /* If a car is in front of us */
           if (car_ahead) {
-              do {
+              if (ref_vel < MAX_VEL) {
                   speed_change += CONSTANT_VEL_VAL;
-              } while (ref_vel < MAX_VEL);
+              } 
 
               if (car_close) {
                   /* To do an incremental change in the velocity, if the car is too close, subtract some
