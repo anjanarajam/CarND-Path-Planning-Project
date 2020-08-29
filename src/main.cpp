9990f67f7bbe7cd4953f26c570798f369846297c
Anjana Rajam <64962809+anjanarajam@users.noreply.github.com>
2020-08-29 17:11:02 +0200

    Bug Fix to avoid jerk and acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -197,25 +197,34 @@ int main() {
           
           /* Behavioral planning : what has to be done based on the predictions */
           /* If a car is in front of us */
-          if (car_ahead) { 
+          if (car_ahead) {
+              do {
+                  speed_change += CONSTANT_VEL_VAL;
+              } while (ref_vel < MAX_VEL);
+
               if (car_close) {
                   /* To do an incremental change in the velocity, if the car is too close, subtract some
                   constant value, 0.224(it ends up being 5 m/second2)*/
                   speed_change -= CONSTANT_VEL_VAL;
+              }
 
-                  /* And if there is no car in the left side of the lane */
-                  if (car_lane > LEFT_LANE && !car_left) {
-                      lane--;
-                  }
-                  /* And if there is no car in the right side of the lane */
-                  else if (car_lane < RIGHT_LANE && !car_right) {
-                      lane++;
-                  }
+              /* And if there is no car in the left side of the lane */
+              if (car_lane > LEFT_LANE && !car_left) {
+                  lane--;
+              }
+              /* And if there is no car in the right side of the lane */
+              else if (car_lane < RIGHT_LANE && !car_right) {
+                  lane++;
+              }
+          
+
+
               /* To do an incremental change in the velocity, add some constant value, 0.224(it ends up being 5 m/second2)
               if there are no cars closeby ego vehicle */
-              } else if (ref_vel < MAX_VEL) {
+/*              } else if (ref_vel < MAX_VEL) {
                     speed_change += CONSTANT_VEL_VAL;
-              }              
+              */
+                       
           } else {
               if (car_lane != lane) {
                   if ((car_lane == LEFT_LANE && !car_right) || (lane == RIGHT_LANE && !car_left)) {
